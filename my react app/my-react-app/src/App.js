cd49965a4ff77bf12f49260388a8bc7cbf4bdbdc
Naata2k <119440543+Naata2k@users.noreply.github.com>
2024-01-30 21:40:53 +0200

    yes

--- my react app/my-react-app/src/App.js
+++ my react app/my-react-app/src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 //import Counter from './component/counter';
-import Timer from './component/timer';
+//import Timer from './component/timer';
+import Prop from './component/props';
 
-
+//<Timer/>
 //<Counter/>
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Timer/>
+        <Prop name="Petteri" age={42} />
       </header>
     </div>
   );
---
+++ my react app/my-react-app/src/component/props.jsx
@@ -0,0 +1,10 @@
+export default function Prop(props){
+    return(
+        <div>
+            <p>Name: {props.name}</p>
+            <p>Age: {props.age}</p>
+        </div>
+    )
+}
+
+
